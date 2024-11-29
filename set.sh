0cf343f2682da131e263307bcdfc108bacec647b
Kyle Aoki <69960581+kyle-aoki@users.noreply.github.com>
2024-11-28 22:50:49 -0500

    O

--- set.sh
+++
@@ -1,6 +0,0 @@
-echo "" >> ~/.zshrc
-echo "#####################################################" >> ~/.zshrc
-echo "#### github.com/kyle-aoki/team-env ##################" >> ~/.zshrc
-echo "source ~/pros/team-env/env.sh" >> ~/.zshrc
-echo "#####################################################" >> ~/.zshrc
-echo "" >> ~/.zshrc
